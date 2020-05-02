b124f872c454feeaf4ec2fff586df382c70c2768
Sean <siang720@gmail.com>
2020-05-02 23:38:37 +0800

    add Sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app" :class="[{ collapsed: collapsed }]">
+    <div class="demo">
+      <div class="container">
+        <router-view />
+      </div>
+      <sidebar-menu
+        :menu="menu"
+        :collapsed="collapsed"
+        :theme="selectedTheme"
+        :show-one-child="true"
+        @toggle-collapse="onToggleCollapse"
+        @item-click="onItemClick"
+      />
+    </div>
   </div>
 </template>
 
@@ -8,6 +20,71 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: "Main Navigation",
+          hiddenOnCollapse: true,
+        },
+        {
+          href: "/",
+          title: "Dashboard",
+          icon: "fa fa-user",
+        },
+        {
+          href: "/service",
+          title: "Service",
+          icon: "fa fa-chart-area",
+          // child: [
+          //   {
+          //     href: "/charts/sublink",
+          //     title: "Sub Link",
+          //   },
+          // ],
+        },
+        {
+          href: "/sendNotify",
+          title: "Send Notify",
+          icon: "fa fa-user",
+        },
+        {
+          href: "/draft",
+          title: "Draft",
+          icon: "fa fa-user",
+        },
+        {
+          href: "/historyNotify",
+          title: "History Notify",
+          icon: "fa fa-user",
+        },
+      ],
+      collapsed: false,
+    };
+  },
+  methods: {
+    onToggleCollapse(collapsed) {
+      console.log(collapsed);
+      this.collapsed = collapsed;
+    },
+  },
 };
 </script>
+
+<style>
+.container {
+  max-width: 900px;
+}
+#app {
+  padding-left: 350px;
+  transition: 0.3s ease;
+}
+#app.collapsed {
+  padding-left: 50px;
+}
+.demo {
+  padding: 50px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import VueSidebarMenu from "vue-sidebar-menu";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
+Vue.use(VueSidebarMenu);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
