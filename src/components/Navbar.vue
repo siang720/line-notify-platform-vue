55192820082c4d81f50bf124e9c829d8af22d668
Sean Liu <siang720@gmail.com>
2020-06-01 16:58:22 +0800

    modify rwd

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,22 @@
 <template>
-  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-transparent">
-    <div id="navbarSupportedContent" class="navbar-collapse collapse">
-      <div class="ml-auto d-flex align-items-center">
+  <nav
+    class="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm fixed-top navbar-dark bg-transparent d-flex justify-content-between"
+  >
+    <div id="navbarSupportedContent" class="navbar-collapse">
+      <div class="navbar-nav ml-auto">
         <!-- is user is login -->
-        <p v-if="isAuthenticated" class="text-dark mr-3 my-auto">{{currentUser.name}} 您好</p>
-        <p v-else class="text-dark mr-3 my-auto">訪客 您好</p>
+        <p
+          v-if="isAuthenticated"
+          id="welcome-text1"
+          class="text-dark mr-3 my-auto"
+        >{{currentUser.name}} 您好</p>
+        <p v-else id="welcome-text2" class="text-dark mr-3 my-auto">訪客 您好</p>
         <router-link v-if="!isAuthenticated" to="/signin" class="text-dark mr-3">登入</router-link>
         <router-link v-if="!isAuthenticated" to="/signup" class="text-dark mr-3">註冊</router-link>
         <button
           v-if="isAuthenticated"
           type="button"
-          class="btn btn-sm btn-outline-success my-2 my-sm-0"
+          class="logout-button btn btn-sm btn-outline-success my-2 my-sm-0"
           @click="logout"
         >登出</button>
       </div>
@@ -36,5 +42,22 @@ export default {
 <style>
 .navbar {
   z-index: 1;
+}
+
+@media (max-width: 600px) {
+  #welcome-text1,
+  #welcome-text2 {
+    display: none;
+  }
+
+  .navbar-nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+
+  .navbar {
+    padding: 0;
+  }
 }
 </style>
